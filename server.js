d4b0e29f4fb3aa9ffd2dbb7862ccd7bfbd80f11e
k-hema16 <hemakondur16@gmail.com>
2025-02-11 11:10:47 +0530

    fourth

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-// server.js
 const express = require('express');
+const cors = require("cors");
 const contactRoutes = require('./routes/contacts');
 const { errorHandler } = require('./middlewares/errorHandler');
+const Database = require('better-sqlite3');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware setup
+app.use(cors());
 app.use(express.json());
+
+// Database setup (initialize before starting the server)
+const db = new Database('contacts.db', { verbose: console.log });
+
+// Routes
 app.use('/contacts', contactRoutes);
 app.use(errorHandler);
-const cors = require("cors");
-app.use(cors());
 
+// Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
